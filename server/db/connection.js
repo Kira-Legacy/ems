85f9373e9671168bfe8d4fbd4a4f17e94c4afd1d
Kira-Legacy <Kirubelwinner@gmail.com>
2025-06-24 09:46:45 +0300

    Successfully connected to database, and create one database called ems

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
+import { config } from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+config({ path: path.resolve(__dirname, "../.env") });
 const uri = process.env.MONGODB_URI;
-
 
 const connectToDatabase = async () => {
   try {
