7567f5c5d1add49fc5f0177c7f34634a403e6c27
Kira-Legacy <Kirubelwinner@gmail.com>
2025-06-24 16:27:49 +0300

    Progress over the login page

---
+++ frontend/src/pages/AdminDashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AdminDashboard = () => {
+    return(
+        <div>AdminDashboard is Here!</div>
+    )
+
+}
+
+export default AdminDashboard;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust based on your project structure
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Savate: ["Sevillana", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
