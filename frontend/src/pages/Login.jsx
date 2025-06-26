ef3adabd075d8e54e2a61d55d3383d4e60dec3c8
Kira-Legacy <Kirubelwinner@gmail.com>
2025-06-26 16:25:56 +0300

    Streak

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,6 +11,7 @@ const Login = () => {
     try {
       const response = await axios.post(
         "http://localhost:5000/api/auth/login",
+
         { email, password }
       );
       console.log(response);
